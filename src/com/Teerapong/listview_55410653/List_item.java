52809060a33b98ed3d0f6d69a92ab23c3328ee6f
Teera653 <sab_za_nk60@hotmail.com>
2014-09-04 18:19:35 +0700

    Listview

---
+++ src/com/Teerapong/listview_55410653/List_item.java
@@ -0,0 +1,20 @@
+package com.Teerapong.listview_55410653;
+
+import android.graphics.drawable.Drawable;
+
+public class List_item {
+	private Drawable drawable;
+	private String title;
+	public Drawable getDrawable() {
+		return drawable;
+	}
+	public void setDrawable(Drawable drawable) {
+		this.drawable = drawable;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+}
